06dc4c829ce31e4766be1a0161aa2371fe05ef9f
mattssonmart <mattsson112@hotmail.com>
2025-10-26 19:44:32 +0100

    text align/caps etc

--- components/formativ.js
+++ components/formativ.js
@@ -2,24 +2,26 @@ class Formativ extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <section class="formativ">
-        <h2 class="underline">Formativ bedömning</h2>
+        <div class="summativ-align">
+        <h2 class="underline">FORMATIV BEDÖMNING</h2>
         <p>Formativ bedömning utvärderar studerandes kunskaper och färdigheter löpande under kursens interaktiva
         lärandemoment. Syftet är att effektivisera bedömningen och samtidigt stödja lärandet. Den formativa
         bedömningen sker i samband med</p>
+        </div>
     <div class="formativ-grid">
         <div>
             <div class="formativ-layout" id="formative01">
-                <h3>Workshops</h3>
+                <h3>WORKSHOPS</h3>
                 <p>Interaktiva undervisningstillfällen där klassen och utbildaren övar tillsammans.</p>
             </div>
             <div class="formativ-layout" id="formative02">
-                <h3>Checkpoints</h3>
+                <h3>CHECKPOINTS</h3>
                 <p>Regelbundna avstämningar där studerande presenterar sina projekt och får direkt
                 återkoppling.</p>
             </div>
         </div>
     </div>
-    <p>Formativ bedömning är ett komplement till summativ bedömning och tillämpas enbart till fördel för studerande.
+    <p class="summativ-align">Formativ bedömning är ett komplement till summativ bedömning och tillämpas enbart till fördel för studerande.
     Studerande kan uppnå kursmål redan innan de summativa examinationerna genom formativ bedömning, men ett
     underkänt resultat kan aldrig enbart baseras på denna bedömningsform</p>
     </section>`;

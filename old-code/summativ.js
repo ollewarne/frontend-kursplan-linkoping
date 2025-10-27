64d5343e93fc8ae85697b610291fe96eebc65abe
mattssonmart <mattsson112@hotmail.com>
2025-10-27 09:26:52 +0100

    code rework. 2 comp/css -> 1 comp/css

---
+++ old-code/summativ.js
@@ -0,0 +1,29 @@
+class Summative extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+         <section class="summativ">
+            <h1 id="kunskap">KUNSKAPSKONTROLL</h1>
+            <div class="summativ-align">
+            <h2 class="underline">SUMMATIV BEDÖMNING</h2>
+            <p>Summativ bedömning utvärderar studerandes kunskaper och färdigheter retroaktivt för att sammanfatta vad
+             den studerande kan vid examinationstillfället. Den summativa bedömningen täcker samtliga kursmål och sker vid
+            följande avgränsade examinationstillfällen:</p>
+            </div>
+            <div class="grid-summativ">
+                <div class="summativ-layout" id="summativ01">
+                    <h3>SKRIFTLIGT KUNSKAPSTEST</h3>
+                    <p>En individuell prövning av teoretiska kunskaper.</p>    
+                </div>
+                <div class="summativ-layout" id="summativ02">
+                    <h3>SKRIFTLIG REFLEKTION</h3>
+                    <p>En individuell redogörelse för egna lösningar och tillvägagångssätt.</p>    
+                </div>
+                <div class="summativ-layout" id="summativ03">
+                    <h3>PROJEKTINLÄMNING</h3>
+                    <p>En gruppinlämning där varje studerandes individuella insats bedöms.</p>    
+                </div>
+            </div>
+        </section>`;
+    }
+}
+customElements.define('summativ-comp', Summative);

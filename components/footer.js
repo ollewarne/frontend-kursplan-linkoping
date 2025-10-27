144daf391718290831ba32e8214d81ca1c58092e
twana.taher@hotmail.com <twana.taher@hotmail.com>
2025-10-27 09:36:44 +0100

    jag ändrade namn på cirklar class , jag added class till mina article och lite annat,

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ class SiteFooter extends HTMLElement {
     this.innerHTML = `
       <footer>
         <div class="footer-content">
-          <img src="images/chas-academy-logo.png" alt="Chas Academy" class="logo-img">
+        
           <p class="footer-text">© 2025 Chas Academy. All rights reserved.</p>
         </div>
       </footer>
